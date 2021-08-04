f5416180c20d3d1c50d155e7a9f2fcece5bfbd0c
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-08-04 04:42:28 -0700

    Update homework.js

--- homework.js
+++ homework.js
@@ -117,4 +117,23 @@ return result
 // task5
 // Write a JavaScript function to get all possible subsets of length 3 of the given array.
 // Assume that all elements in the array are unique.
+
+let array = [5, 9, 23, 0, -2, -1]
+function getAllPossibleSubsets(arr){
+    if (arr.length < 3){
+    return arr
+    }
+    let a = '';
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++){
+            for (let k = j + 1; k < arr.length; k++){
+                a = "[ " + arr[i] + ", " + arr[j] + "," + arr[k] + " ]"
+                result.push(a)
+            }  
+        }
+    }
+    return result
+}
+console.log(getAllPossibleSubsets(array))
     
